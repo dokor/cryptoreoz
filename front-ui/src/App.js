61c8334592c76cbd210f76d8fa08a61c5a24d383
lairt <tlair@coreoz.com>
2021-02-09 15:11:55 +0100

    init react-admin

--- front-ui/src/App.js
+++ front-ui/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Admin, ListGuesser, Resource } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
 
 function App() {
+  const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Admin dataProvider={dataProvider}>
+      <Resource name="users" list={ListGuesser}/>
+    </Admin>
   );
 }
 
